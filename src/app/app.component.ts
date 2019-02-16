9538eaf97d3fe269cc283c1cb6e50c4671e267aa
Raymond981 <163163@ids.upchiapas.edu.mx>
2019-02-16 12:25:01 -0600

    Push BASE
    Primera base para la implementación de push notification. v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import * as firebase from 'firebase'; 
 
+import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
 var config = {
-  apiKey: "AIzaSyBzJnWf-KlIDmn-_85fwmrl35BJUN1Pgy0",
-  authDomain: "pokemon-b1b27.firebaseapp.com",
-  databaseURL: "https://pokemon-b1b27.firebaseio.com",
-  projectId: "pokemon-b1b27",
-  storageBucket: "",
-  messagingSenderId: "934452340257"
+  apiKey: "AIzaSyD8ji6B5BfBa7I6mQ5KGMOfruu7dyBTyV0",
+  authDomain: "tallerfirebase-6a4ab.firebaseapp.com",
+  databaseURL: "https://tallerfirebase-6a4ab.firebaseio.com",
+  projectId: "tallerfirebase-6a4ab",
+  storageBucket: "tallerfirebase-6a4ab.appspot.com",
+  messagingSenderId: "831981256313"
 };
 
 @Component({
@@ -22,7 +23,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private push: Push
   ) {
     this.initializeApp();
   }
@@ -33,5 +35,29 @@ export class AppComponent {
       this.splashScreen.hide();
     });
     firebase.initializeApp(config);
+    this.notificacion();
+  }
+
+  notificacion(){
+    const options: PushOptions = {
+      android: {
+        senderID: '831981256313'
+      },
+      ios: {
+          alert: 'true',
+          badge: true,
+          sound: 'false'
+      }
+   }
+   
+   const pushObject: PushObject = this.push.init(options);
+   
+   
+   pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+   
+   pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+   
+   pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+   
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Push } from '@ionic-native/push/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+    SplashScreen,Push,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
